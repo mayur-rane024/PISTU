bdde72b3d88cd27cd1ca37982b379969e11fdce7
Parikshit Rajpurohit <parikshitrajpurohit56@gmail.com>
2025-07-03 21:07:56 +0530

    bug of browserRouter removed

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 import Home from "./pages/HeroPage/Hero";
